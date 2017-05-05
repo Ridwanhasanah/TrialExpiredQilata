857d5b540091c6d2c72c45d49fdd09a44308df74
Ridwan Hasanah <sun.riseagain@yahoo.com>
2017-05-05 14:58:21 +0700

    Penambahan link untuk di klik

--- TrialExpiredQilata.php
+++ TrialExpiredQilata.php
@@ -17,17 +17,14 @@ function rhnotice() {
 
 	$blogs_id = get_current_blog_id();
 	$user_id  = get_current_user_id();
+	$atau = 13 || $user_id == 1;
 
 	global $wpdb;
 	$table = $wpdb->prefix."blogs";
 
 	$users = $wpdb->get_results("SELECT registered FROM wp_blogs WHERE blog_id = $blogs_id ");
-	$domain = $wpdb->get_results("SELECT domain FROM wp_blogs WHERE blog_id = $blogs_id ");
-	$qilatab = 'qd';
-	$qilata = 'www.qilata.com';
-	$checkdomain = strpos($domain,$qilatab);
 
-	if ($qilatab == $checkdomain || $qilata) { //chek domain punya siapa
+	if ($blogs_id == 1 ||$blogs_id == 36 ||$blogs_id == 37||$blogs_id == 39 ||$blogs_id == 40||$blogs_id == 41||$blogs_id == 42||$blogs_id == 43||$blogs_id == 44||$blogs_id == 45||$blogs_id == 46||$blogs_id == 47||$blogs_id ==54 ||$blogs_id == 55||$blogs_id ==56 ||$blogs_id == 57 ||$blogs_id == 58 ||$blogs_id == 59) { //check id blog
 		?>
 		<div id="message" class="error notice notice-error is-dismissible">
 			<p><?php _e( 'Ini WEBSITE QILATA.', 'sample-text-domain' ); ?></p>
@@ -64,7 +61,7 @@ function rhnotice() {
 			}else{
 				?>
 				<div id="message" class="error notice notice-error is-dismissible">
-					<p><?php _e( 'Masa Trial Anda '.$selisih.' lagi dan  akan berakhir pada tanggal ' . $trial2 . '.', 'sample-text-domain' ); ?></p>
+					<p><strong><a href="http://www.qilata.com" target="_blank"><?php _e( 'Masa Trial Anda '.$selisih.' lagi, Upgrade sekarang Klik di sini !!! .', 'sample-text-domain' ); ?></a></strong></p>
 				</div>
 				<?php
 			}
